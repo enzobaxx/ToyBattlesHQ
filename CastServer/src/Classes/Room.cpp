cb2437b37bdede3775541ca6b0284b24db7c25ee
pigreco <tony.kolarek@student.supsi.ch>
2026-03-24 01:56:33 +0100

    Reduce max positional packet size

--- CastServer/src/Classes/Room.cpp
+++ CastServer/src/Classes/Room.cpp
@@ -396,7 +396,7 @@ namespace Cast
 		{
 			if (m_pendingPositions.empty()) return;
 
-			static std::array<std::uint8_t, 2048> batchBuffer;
+			static std::array<std::uint8_t, 1450> batchBuffer;
 			std::size_t index = 0;
 
 			while (index < m_pendingPositions.size())
@@ -409,7 +409,7 @@ namespace Cast
 					auto& pkt = m_pendingPositions[index];
 					const auto size = pkt.getDataSize();
 
-					if (totalSize + size > 2032) // 8 bytes header + 4 bytes roomtick
+					if (totalSize + size > 1436) // 8 bytes header + 4 bytes roomtick
 						break;
 
 					std::memcpy(batchBuffer.data() + totalSize, pkt.getData(), size);
@@ -421,7 +421,7 @@ namespace Cast
 				std::memcpy(batchBuffer.data(), &m_roomTick, sizeof(m_roomTick));
 				totalSize += sizeof(m_roomTick);
 
-				static Common::Network::UnecryptedPacket batch(2048, 322, 0);
+				static Common::Network::UnecryptedPacket batch(1450, 322, 0);
 				batch.setOption(static_cast<uint32_t>(count));
 				batch.setData(batchBuffer.data(), static_cast<uint16_t>(totalSize));
 
