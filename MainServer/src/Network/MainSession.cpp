7feb0cbe5ca2a362b29424b4431c2011120e02fe
SoWeBegin <36608065+SoWeBegin@users.noreply.github.com>
2026-03-31 22:23:34 +0200

    Update MainSession.cpp

--- MainServer/src/Network/MainSession.cpp
+++ MainServer/src/Network/MainSession.cpp
@@ -1880,7 +1880,7 @@ namespace Main
 			}
 			
 			auto& currentTotal = m_eventMissions[index];  
-			if (currentTotal < Common::Constants::eventMissionTotal)
+			if (currentTotal < Common::Constants::eventMissionTotal - 1)
 			{
 				sendMessage("Obtained one point for event mission idx: " + std::to_string(index));
 
