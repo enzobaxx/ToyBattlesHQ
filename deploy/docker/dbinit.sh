b9a6f43bbb1b0d0003c16ee9614dc8b2d3be186c
pigreco <tony.kolarek@student.supsi.ch>
2026-06-01 19:11:35 +0200

    Update CD imports

--- deploy/docker/dbinit.sh
+++ deploy/docker/dbinit.sh
@@ -28,7 +28,7 @@ else
     echo "[dbinit] Creating database 'microvolts-db'..."
     "${CLIENT[@]}" -e "CREATE DATABASE IF NOT EXISTS \`microvolts-db\`;"
     echo "[dbinit] Importing schema and seed data..."
-    sed -E '/\/\*![0-9]+ SET (@OLD_TIME_ZONE|TIME_ZONE)/d' /app/emu/microvolts-db.sql | "${CLIENT[@]}" microvolts-db
+    sed -E '/\/\*![0-9]+ SET [A-Za-z_]+[[:space:]]*=[[:space:]]*@OLD_/d' /app/emu/microvolts-db.sql | "${CLIENT[@]}" microvolts-db
     echo "[dbinit] Setting root password and TCP access..."
     "${CLIENT[@]}" <<SQL
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PW}';
